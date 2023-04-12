fd96b41fcff7737cfd839df0c87f31cbf36586be
saravaneshwaran <123050936+saravaneshwaran@users.noreply.github.com>
2023-04-12 11:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function foo(){
+    let firstname=document.getElementById("firstname").value;
+    console.log(`Fistname:${firstname}`);
+    let Middlename=document.getElementById("Middlename").value;
+    console.log(`Middlename:${Middlename}`);
+    let lastname=document.getElementById("lastname").value;
+    console.log(`Lastname:${lastname}`);
+    let mail=document.getElementById("mail").value;
+    console.log(`Mail:${mail}`);
+    let Address=document.getElementById("Address").value;
+    console.log(`Address:${Address}`);
+}
